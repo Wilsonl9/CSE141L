ce0f8cae8bdfda482e005c44ea6d7eb65e12e1b2
nbasiago <nbasiago@ucsd.edu>
2018-02-22 16:58:52 -0800

    alu should work now?
    This should work, but top.sv has lots of errors and idk what that is

--- alu.sv
+++ alu.sv
@@ -11,20 +11,24 @@ module alu (
   output logic      z, 		    // zero flag, like ARM Z flag
   output logic      neg);          // negative flag
   op_mne op_mnemonic;			    // type enum: used for convenient waveform viewing
-
-  logic[7:0] sum;
   
+  logic [8:0] temp;
   always_comb begin
     case(op)						// selective override one or more defaults
 	   kADD: begin
-		        {co,acc} = in_acc + in_a + ci;  // add w/ carry in and out
-              sum <= in_acc + in_a + ci;
-				  neg = sum[7];
-				end
+			temp = in_acc + in_a + ci;
+			assign co = temp[8];
+			assign neg = temp[7];
+			assign acc = temp[7:0];
+		        //{co,acc} = in_acc + in_a + ci;  // add w/ carry in and out
+              		//sum <= in_acc + in_a + ci;
+				//  neg = sum[7];
+		 end
       kSUB: begin
-		        {co,acc} = in_acc + (!in_a + 1);
-				  sum <= in_acc + (!in_a + 1);
-				  neg = sum[7];
+				  temp = in_acc + (!in_a + 1);
+		        assign co = temp[8];
+				  assign neg = temp[7];
+			     assign acc = temp[7:0];
 				end
  	   kSTR: acc = in_acc;			  // store in data_mem from reg_file
 	   kLDR: acc = in_a;		        // load reg_file from data_mem
